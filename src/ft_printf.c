53582183d9556f50357ba5f0b33e6ab9529a1a9c
Lucas ADAWI <ladawi@e1r11p3.42.fr>
2020-01-19 15:02:56 +0100

    vive la norm, done

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ladawi <ladawi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 18:04:14 by ladawi            #+#    #+#             */
-/*   Updated: 2020/01/18 17:48:07 by ladawi           ###   ########.fr       */
+/*   Updated: 2020/01/19 15:02:26 by ladawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 #include <stdio.h>
 #include "../libft/libft.h"
 
+void	ft_inityeet(t_struct *yeet, char *str)
+{
+	yeet->index = 0;
+	yeet->retcount = 0;
+	yeet->str = (char *)str;
+	yeet->strid = 0;
+	yeet->precision = -1;
+	ft_memset(yeet->buff, 0, B_SIZE);
+}
+
 int		ft_printf(const char *str, ...)
 {
 	va_list		ap;
 	t_struct	yeet;
 
 	va_start(ap, str);
-	yeet.index = 0;
-	yeet.retcount = 0;
-	yeet.str = (char *)str;
-	yeet.strid = 0;
-	yeet.precision = -1;
-	ft_memset(yeet.buff, 0, B_SIZE);
+	ft_inityeet(&yeet, str);
 	ft_scanstr(&yeet, ap);
 	write(1, yeet.buff, yeet.index);
 	va_end(ap);
